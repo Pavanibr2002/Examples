d2fbee13b787eb55c6825415c192869b14e0583c
Pavanibr2002 <130268066+Pavanibr2002@users.noreply.github.com>
2023-11-05 10:48:27 +0530

    First Commit

---
+++ src/Example/helloworld.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author B R PAVANI
+ *
+ */
+module Example {
+}
